afd0a7676fc0d544bcf1712bfcfd2557c97199b2
Brad Buran <bburan@alum.mit.edu>
2018-04-18 15:44:51 -0700

    Remove obsolete import

--- psi/controller/calibration/tone.py
+++ psi/controller/calibration/tone.py
@@ -8,7 +8,6 @@ import numpy as np
 import pandas as pd
 
 from .util import tone_power_conv, db
-from ..util import acquire
 from . import (FlatCalibration, PointCalibration, CalibrationTHDError,
                CalibrationNFError)
 from ..queue import FIFOSignalQueue
