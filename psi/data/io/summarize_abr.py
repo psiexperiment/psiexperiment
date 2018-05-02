d949ac0aa2e1ae56e01d94d863d485d133871062
Brad Buran <bburan@alum.mit.edu>
2018-05-02 13:43:59 -0700

    Skip ABR file if error and print message

--- psi/data/io/summarize_abr.py
+++ psi/data/io/summarize_abr.py
@@ -12,32 +12,35 @@ def _main(args):
     columns = ['frequency', 'level', 'polarity']
     for filename in args.filenames:
         print('Processing {}'.format(filename))
-        fh = abr.load(filename)
-        epochs = fh.get_epochs_filtered(offset=args.offset,
-                                        duration=args.duration,
-                                        filter_lb=args.filter_lb,
-                                        filter_ub=args.filter_ub,
-                                        columns=columns)
+        try:
+            fh = abr.load(filename)
+            epochs = fh.get_epochs_filtered(offset=args.offset,
+                                            duration=args.duration,
+                                            filter_lb=args.filter_lb,
+                                            filter_ub=args.filter_ub,
+                                            columns=columns)
 
-        file_template = template.format(args.offset*1e3,
-                                        (args.offset+args.duration)*1e3,
-                                        args.filter_lb, args.filter_ub)
-        file_template = os.path.join(filename, file_template)
+            file_template = template.format(args.offset*1e3,
+                                            (args.offset+args.duration)*1e3,
+                                            args.filter_lb, args.filter_ub)
+            file_template = os.path.join(filename, file_template)
 
-        # Apply the reject
-        reject_threshold = fh.trial_log.at[0, 'reject_threshold']
-        m = epochs < reject_threshold
-        m = m.all(axis=1)
-        epochs = epochs.loc[m]
+            # Apply the reject
+            reject_threshold = fh.trial_log.at[0, 'reject_threshold']
+            m = epochs < reject_threshold
+            m = m.all(axis=1)
+            epochs = epochs.loc[m]
 
-        epoch_reject_ratio = 1-m.groupby(columns[:-1]).mean()
-        epoch_mean = epochs.groupby(columns).mean().groupby(columns[:-1]).mean()
-        epoch_n = epochs.groupby(columns[:-1]).size()
+            epoch_reject_ratio = 1-m.groupby(columns[:-1]).mean()
+            epoch_mean = epochs.groupby(columns).mean().groupby(columns[:-1]).mean()
+            epoch_n = epochs.groupby(columns[:-1]).size()
 
-        # Write the data to files
-        epoch_reject_ratio.unstack().to_csv(file_template.format('reject ratio'))
-        epoch_n.unstack().to_csv(file_template.format('number of epochs'))
-        epoch_mean.T.to_csv(file_template.format('average waveform'))
+            # Write the data to files
+            epoch_reject_ratio.unstack().to_csv(file_template.format('reject ratio'))
+            epoch_n.unstack().to_csv(file_template.format('number of epochs'))
+            epoch_mean.T.to_csv(file_template.format('average waveform'))
+        except:
+            print('Error processing {}'.format(filename))
 
 
 def main():
@@ -55,7 +58,7 @@ def main():
                         default=3000)
 
     args = parser.parse_args()
-    main(args)
+    _main(args)
 
 
 if __name__ == '__main__':
