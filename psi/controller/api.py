bac91f477d1985d29194d90989662fc61df6ece2
Brad Buran <bburan@alum.mit.edu>
2018-05-03 14:52:47 -0700

    Fix circular import

--- psi/controller/api.py
+++ psi/controller/api.py
@@ -3,7 +3,3 @@ from .input import Input
 from .output import (ContinuousOutput, EpochOutput, Trigger, Toggle)
 from .experiment_action import (ExperimentAction, ExperimentEvent,
                                 ExperimentState)
-
-import enaml
-with enaml.imports():
-    from .base_manifest import BaseManifest
