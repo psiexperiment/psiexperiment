e8a513a2ac551808b959389b76de815676849a97
Brad Buran <bburan@alum.mit.edu>
2018-06-18 15:03:59 -0700

    Added support for reading continuous/epoch data

--- psi/data/io/bcolz_tools.py
+++ psi/data/io/bcolz_tools.py
@@ -2,19 +2,77 @@ import os.path
 
 import bcolz
 import numpy as np
+import pandas as pd
 
 
-class EpochData:
+class Dataset:
 
-    def __init__(self, base_path, epoch_name):
-        epoch_path = os.path.join(base_path, epoch_name)
-        epoch_md_path = os.path.join(base_path,
-                                     '{}_metadata'.format(epoch_name))
-        self.epoch = bcolz.carray(rootdir=epoch_path)
-        self.epoch_md = bcolz.ctable(rootdir=epoch_md_path)
+    def __init__(self, base_path, continuous_names=None, epoch_names=None):
+        self.continuous = {}
+        if continuous_names is not None:
+            for name in continuous_names:
+                path = os.path.join(base_path, name)
+                self.continuous[name] = bcolz.carray(rootdir=path)
 
-    def get_groups(self, groups):
-        return get_epoch_groups(self.epoch, self.epoch_md, groups)
+        self.epoch = {}
+        self.epoch_md = {}
+        if epoch_names is not None:
+            for name in epoch_names:
+                # Open the epoch
+                path = os.path.join(base_path, name)
+                self.epoch[name] = bcolz.carray(rootdir=path)
+                # Load the metadata
+                path = os.path.join(base_path, f'{name}_metadata')
+                md = bcolz.ctable(rootdir=path)
+                self.epoch_md[name] = md.todataframe()
+
+    def extract_epochs_df(self, signal_name, epoch_name, columns=None, offset=0,
+                          duration=None, padding_samples=0):
+        return get_epochs_df(
+            self.continuous[signal_name],
+            self.epoch_md[epoch_name],
+            columns=columns,
+            offset=offset,
+            duration=duration,
+            padding_samples=padding_samples
+        )
+
+
+def get_epochs_df(continuous, epoch_md, columns, offset=0, duration=None,
+                  padding_samples=0):
+
+    fs = continuous.attrs['fs']
+    if columns is None:
+        columns = []
+    columns = list(columns) + ['t0']
+    keys = []
+    data = []
+    max_samples = continuous.shape[-1]
+
+    for _, row in epoch_md.iterrows():
+        if duration is None:
+            duration = row['duration']
+        key = tuple(row[c] for c in columns)
+        keys.append(key)
+        t0 = row['t0']
+        lb = round((t0+offset)*fs)
+        ub = lb + round(duration*fs)
+        lb -= padding_samples
+        ub += padding_samples
+
+        d = continuous[lb:ub]
+
+        if lb < 0 or ub > max_samples:
+            lb_pad = max(0-lb, 0)
+            ub_pad = max(ub-max_samples, 0)
+            d = np.pad(d, (lb_pad, ub_pad), mode='constant',
+                       constant_values=np.nan)
+
+        t = pd.Index(np.arange((ub-lb))/fs, name='time')
+        d = pd.Series(d, index=t, name='signal')
+        data.append(d)
+
+    return pd.concat(data, keys=keys, names=columns).unstack('time')
 
 
 def get_epoch_groups(epoch, epoch_md, groups):
