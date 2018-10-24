a6f53134699e62750e37134b52e111cf23e44553
Brad Buran <bburan@alum.mit.edu>
2018-10-24 10:03:54 -0700

    Add function back in for speaker_calibration

--- psi/data/io/bcolz_tools.py
+++ psi/data/io/bcolz_tools.py
@@ -185,3 +185,22 @@ class BcolzRecording(Recording):
         else:
             raise AttributeError(name)
 
+
+def get_epoch_groups(epoch, epoch_md, groups):
+    # Used by speaker_calibration
+    fs = epoch.attrs['fs']
+    df = epoch_md.todataframe()
+    df['samples'] = np.round(df['duration']*fs).astype('i')
+    df['offset'] = df['samples'].cumsum() - df.loc[0, 'samples']
+
+    epochs = {}
+    for keys, g_df in df.groupby(groups):
+        data = []
+        for _, row in g_df.iterrows():
+            o = row['offset']
+            s = row['samples']
+            d = epoch[o:o+s][np.newaxis]
+            data.append(d)
+        epochs[keys] = np.concatenate(data, axis=0)
+
+    return epochs
