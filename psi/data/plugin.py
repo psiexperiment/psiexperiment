2fddf21b34eee49e8f89aca918b99e8de5e6a6c6
Brad Buran <bburan@alum.mit.edu>
2019-02-26 15:18:40 -0800

    More sane naming scheme for data plugin attributes

--- psi/data/plugin.py
+++ psi/data/plugin.py
@@ -24,8 +24,7 @@ PLOT_POINT = 'psi.data.plots'
 class DataPlugin(Plugin):
 
     _sinks = Typed(list, [])
-    _plots = Typed(list, [])
-    _containers = Typed(dict, {})
+    _containers = Typed(list, [])
 
     inputs = Typed(dict, {})
     context_info = Typed(dict, {})
@@ -59,12 +58,12 @@ class DataPlugin(Plugin):
         self._sinks = sinks
 
     def _refresh_plots(self, event=None):
-        plots = []
+        containers = []
         point = self.workbench.get_extension_point(PLOT_POINT)
         for extension in point.extensions:
-            plots.extend(extension.get_children(PlotContainer))
-        load_manifests(plots, self.workbench)
-        self._plots = plots
+            containers.extend(extension.get_children(PlotContainer))
+        load_manifests(containers, self.workbench)
+        self._containers = containers
 
     def _bind_observers(self):
         self.workbench.get_extension_point(SINK_POINT) \
@@ -138,16 +137,25 @@ class DataPlugin(Plugin):
         raise AttributeError('Source {} not available'.format(source_name))
 
     def find_plot_container(self, plot_container_name):
-        for plot in self._plots:
-            if plot.name == plot_container_name:
-                return plot
+        for container in self._containers:
+            if container.name == plot_container_name:
+                return containers
         m = 'Plot container {} not available'
         raise AttributeError(m.format(plot_container_name))
 
     def find_viewbox(self, viewbox_name):
-        for plot in self._plots:
-            for viewbox in plot.children:
+        for container in self._containers:
+            for viewbox in container.children:
                 if viewbox.name == viewbox_name:
                     return viewbox
         m = 'Viewbox {} not available'
         raise AttributeError(m.format(viewbox_name))
+
+    def find_plot(self, plot_name):
+        for container in self._containers:
+            for viewbox in container.children:
+                for plot in viewbox.children:
+                    if plot.name == plot_name:
+                        return plot
+        m = 'Plot {} not available'
+        raise AttributeError(m.format(plot_name))
