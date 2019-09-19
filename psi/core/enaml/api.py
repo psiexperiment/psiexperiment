7fa2bc6d484c22dc38f77f5ce07fd96975afc5c3
Brad Buran <bburan@alum.mit.edu>
2019-09-19 16:09:57 -0700

    Add missing import to api

--- psi/core/enaml/api.py
+++ psi/core/enaml/api.py
@@ -2,7 +2,8 @@ import logging
 log = logging.getLogger(__name__)
 
 from .contribution import PSIContribution
-from .editable_table_widget import DataFrameTable, EditableTable, ListDictTable
+from .editable_table_widget import (DataFrameTable, EditableTable, ListTable,
+                                    ListDictTable)
 from .manifest import PSIManifest
 from .util import (load_enaml_module_from_file, load_manifest, load_manifests,
                    load_manifest_from_file)
