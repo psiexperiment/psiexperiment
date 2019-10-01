9ec384261028c59016ad6913d2509bbc8dfdd9e8
Brad Buran <buran@ohsu.edu>
2019-10-01 13:53:08 -0700

    Add support for loading prior preferences

--- psi/application/base_launcher.py
+++ psi/application/base_launcher.py
@@ -2,6 +2,7 @@ import logging
 log = logging.getLogger(__name__)
 
 from functools import partial
+import datetime as dt
 import os.path
 from pathlib import Path
 import subprocess
@@ -14,6 +15,7 @@ with enaml.imports():
     from psi.application.base_launcher_view import LauncherView
 
 from psi import get_config
+from psi.util import get_tagged_values
 from psi.application import list_calibrations, list_io, list_preferences
 from psi.application.experiment_description import get_experiments, ParadigmDescription
 
@@ -21,19 +23,22 @@ from psi.application.experiment_description import get_experiments, ParadigmDesc
 class SimpleLauncher(Atom):
 
     io = Typed(Path)
-    experiment = Typed(ParadigmDescription)
+    experiment = Typed(ParadigmDescription).tag(template=True, required=True)
     calibration = Typed(Path)
     preferences = Typed(Path)
     save_data = Bool(True)
-    experimenter = Unicode()
-    note = Unicode()
+    experimenter = Unicode().tag(template=True, required=True)
+    note = Unicode().tag(template=True)
 
     experiment_type = Unicode()
     experiment_choices = List()
 
     root_folder = Typed(Path)
     base_folder = Typed(Path)
+    wildcard = Unicode()
     template = '{{date_time}} {experimenter} {note} {experiment}'
+    wildcard_template = '*{experiment}'
+    use_prior_preferences = Bool(False)
 
     can_launch = Bool(False)
 
@@ -53,7 +58,6 @@ class SimpleLauncher(Atom):
     def _update_choices(self):
         self._update_available_calibrations()
         self._update_available_preferences()
-
 
     def _update_available_calibrations(self):
         self.available_calibrations = list_calibrations(self.io)
@@ -113,9 +117,9 @@ class SimpleLauncher(Atom):
             self.base_folder = None
             return
 
-        exclude = ['preferences', 'save_data', 'base_folder', 'can_launch']
-        vals = {m: getattr(self, m) for m in self.members() if m not in exclude}
-        for k, v in vals.items():
+        template_vals = get_tagged_values(self, 'template')
+        required_vals = get_tagged_values(self, 'required')
+        for k, v in required_vals.items():
             if k == 'note':
                 continue
             if not v:
@@ -123,9 +127,29 @@ class SimpleLauncher(Atom):
                 self.base_folder = None
                 return
 
-        vals['experiment'] = vals['experiment'].name
-        self.base_folder = self.root_folder / self.template.format(**vals)
+        template_vals['experiment'] = template_vals['experiment'].name
+        self.base_folder = self.root_folder / self.template.format(**template_vals)
+        self.wildcard = self.wildcard_template.format(**template_vals)
         self.can_launch = True
+
+    def get_preferences(self):
+        if not self.use_prior_preferences:
+            return self.preferences
+        options = []
+        for match in self.root_folder.glob(self.wildcard):
+            if (match / 'final.preferences').exists():
+                n = match.name.split(' ')[0]
+                date = dt.datetime.strptime(n, '%Y%m%d-%H%M%S')
+                options.append((date, match / 'final.preferences'))
+            elif (match / 'initial.preferences').exists():
+                n = match.name.split(' ')[0]
+                date = dt.datetime.strptime(n, '%Y%m%d-%H%M%S')
+                options.append((date, match / 'initial.preferences'))
+        options.sort(reverse=True)
+        if len(options):
+            return options[0][1]
+        m = f'Could not find prior preferences for {self.experiment_type}'
+        raise ValueError(m)
 
     def launch_subprocess(self):
         args = ['psi', self.experiment.name]
@@ -133,7 +157,7 @@ class SimpleLauncher(Atom):
         if self.save_data:
             args.append(str(self.base_folder))
         if self.preferences:
-            args.extend(['--preferences', str(self.preferences)])
+            args.extend(['--preferences', str(self.get_preferences())])
         if self.io:
             args.extend(['--io', str(self.io)])
         if self.calibration:
@@ -148,9 +172,10 @@ class SimpleLauncher(Atom):
 
 class AnimalLauncher(SimpleLauncher):
 
-    animal = Unicode()
+    animal = Unicode().tag(template=True, required=True)
 
     template = '{{date_time}} {experimenter} {animal} {note} {experiment}'
+    wildcard_template = '*{animal}*{experiment}'
 
     def _observe_animal(self, event):
         self._update()
@@ -158,9 +183,10 @@ class AnimalLauncher(SimpleLauncher):
 
 class EarLauncher(AnimalLauncher):
 
-    ear = Enum('right', 'left')
+    ear = Enum('right', 'left').tag(template=True)
 
     template = '{{date_time}} {experimenter} {animal} {ear} {note} {experiment}'
+    wildcard_template = '*{animal} {ear}*{experiment}'
 
     def _observe_ear(self, event):
         self._update()
