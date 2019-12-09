ca7050718a402326259ed6b68b94c53f8d2a2be8
Brad Buran <bburan@alum.mit.edu>
2019-12-09 15:44:00 -0800

    Fix bug in cos2envelope
    cos2envelope was not working when the start of the envelope was
    greater than 0. Since this wasn't a commonly-used parameter and
    does not affect any of the current paradigms, there should be
    no issues.
    
    Added unit-test to catch future regressions.

--- tests/test_token.py
+++ tests/test_token.py
@@ -1,10 +1,43 @@
 import pytest
 import numpy as np
+import matplotlib.pyplot as plt
 
 import enaml
 with enaml.imports():
-    from psi.token.primitives import ToneFactory, Cos2EnvelopeFactory
+    from psi.token.primitives import (cos2envelope, Cos2EnvelopeFactory,
+                                      SilenceFactory, ToneFactory)
     from psi.controller.calibration.api import InterpCalibration
+
+
+def test_cos2envelope():
+    fs = 10e3
+    duration = 10e-3
+    tone_duration = 5e-3
+    rise_time = 0.5e-3
+    samples = round(duration*fs)
+
+    # Make sure that the envelope is identical even if we delay the start
+    y0 = cos2envelope(fs, 0, samples, 0, rise_time, tone_duration)
+    for offset in (0.1e-3, 0.2e-3, 0.3e-3):
+        y1 = cos2envelope(fs, 0, samples, offset, rise_time, tone_duration)
+        n = round(offset * fs)
+        np.testing.assert_allclose(y0[:-n], y1[n:])
+
+    # Now, test piecemeal generation
+    partition_size = 0.1e-3
+    partition_samples = round(partition_size * fs)
+    n_partitions = round(samples / partition_samples)
+    for offset in (0, 0.1e-3, 0.2e-3, 0.3e-3):
+        print(offset)
+        env = Cos2EnvelopeFactory(fs, offset, rise_time, tone_duration,
+                                SilenceFactory(fill_value=1))
+        y1 = [env.next(partition_samples) for i in range(n_partitions)]
+        y1 = np.concatenate(y1)
+        n = round(offset * fs)
+        if n > 0:
+            np.testing.assert_allclose(y0[:-n], y1[n:])
+        else:
+            np.testing.assert_allclose(y0, y1)
 
 
 def test_token_generation():
@@ -54,4 +87,4 @@ def test_token_generation():
 
     t = np.arange(fs, dtype=np.float32)/fs
     expected = np.cos(2*np.pi*t*100)*np.sqrt(2)
-    np.testing.assert_array_equal(waveform, expected)
+    np.testing.assert_allclose(waveform, expected, atol=2e-4)
