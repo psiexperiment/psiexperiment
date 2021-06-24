2dec532d8886472b42b618e48cf80b768dbdefbe
Brad Buran <info@bradburan.com>
2021-06-24 11:17:11 -0700

    Add ExperimentManifest

--- psi/core/enaml/api.py
+++ psi/core/enaml/api.py
@@ -1,10 +1,15 @@
 import logging
 log = logging.getLogger(__name__)
 
+import enaml
+
 from .contribution import PSIContribution
 from .editable_table_widget import (DataFrameTable, EditableTable, ListTable,
                                     ListDictTable)
-from .manifest import ExperimentManifest, PSIManifest
+
+with enaml.imports():
+    from .manifest import ExperimentManifest, PSIManifest
+
 from .util import (load_enaml_module_from_file, load_manifest, load_manifests,
                    load_manifest_from_file)
 
