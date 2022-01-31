8a4e14a0f710cd5c702451f25dc78890e926eb62
Brad Buran <buran@ohsu.edu>
2022-01-31 13:53:35 -0800

    Update to reflect new calibration API

--- psi/controller/channel.py
+++ psi/controller/channel.py
@@ -62,7 +62,7 @@ class Channel(PSIContribution):
         return f'{self.type_code}::{self.name}'
 
     def _default_calibration(self):
-        return UnityCalibration()
+        return FlatCalibration.unity()
 
     def __init__(self, *args, **kwargs):
         # This is a hack due to the fact that name is defined as a Declarative
