5f0b9b252465b61bdc7b1f10c5cb565609955333
Brad Buran <buran@ohsu.edu>
2022-02-23 11:59:57 -0800

    Update behavior descriptions

--- psi/paradigms/descriptions/behavior.py
+++ psi/paradigms/descriptions/behavior.py
@@ -8,15 +8,16 @@ CORE_PATH = 'psi.paradigms.core.'
 ParadigmDescription(
     'appetitive_gonogo_food', 'Appetitive GO-NOGO food', 'animal', [
         (PATH + 'behavior_np_gonogo.BehaviorManifest',),
-        (PATH + 'behavior_mixins.PelletDispenserMixinManifest',),
+        (PATH + 'behavior_mixins.PelletDispenser',),
     ],
 )
 
 
 ParadigmDescription(
     'auto_gonogo', 'Auto GO-NOGO', 'animal', [
-        (PATH + 'behavior_mixins.BaseGoNogoMixin',),
         (PATH + 'behavior_auto_gonogo.BehaviorManifest',),
+        (PATH + 'behavior_mixins.BaseGoNogoMixin',),
+        (PATH + 'behavior_mixins.WaterBolusDispenser',),
         (CORE_PATH + 'microphone_mixins.MicrophoneSignalViewManifest',),
         (CORE_PATH + 'microphone_mixins.MicrophoneFFTViewManifest',),
     ],
