4696727860cfa5b07da692132948677db35e9439
Brad Buran <buran@ohsu.edu>
2022-03-15 07:02:15 -0700

    Add amplifier calibration
    This experiment can be used to calibrate an external
    amplifier knob for gain

--- psi/paradigms/descriptions/calibration.py
+++ psi/paradigms/descriptions/calibration.py
@@ -2,11 +2,13 @@ from psi.experiment.api import ParadigmDescription
 
 
 PATH = 'psi.paradigms.calibration.'
+CORE_PATH = 'psi.paradigms.core.'
 
 
 ParadigmDescription(
     'speaker_calibration_golay', 'Speaker calibration (Golay)', 'calibration', [
         (PATH + 'speaker_calibration.BaseSpeakerCalibrationManifest',),
+        (PATH + 'calibration_mixins.ChannelSettingMixins',),
         (PATH + 'calibration_mixins.GolayMixin',),
         (PATH + 'calibration_mixins.ToneValidateMixin',),
     ]
@@ -16,6 +18,7 @@ ParadigmDescription(
 ParadigmDescription(
     'speaker_calibration_chirp', 'Speaker calibration (chirp)', 'calibration', [
         (PATH + 'speaker_calibration.BaseSpeakerCalibrationManifest',),
+        (PATH + 'calibration_mixins.ChannelSettingMixins',),
         (PATH + 'calibration_mixins.ChirpMixin',),
         (PATH + 'calibration_mixins.ToneValidateMixin',),
     ]
@@ -25,6 +28,7 @@ ParadigmDescription(
 ParadigmDescription(
     'speaker_calibration_tone', 'Speaker calibration (tone)', 'calibration', [
         (PATH + 'speaker_calibration.BaseSpeakerCalibrationManifest',),
+        (PATH + 'calibration_mixins.ChannelSettingMixins',),
         (PATH + 'calibration_mixins.ToneMixin',),
     ]
 )
@@ -33,5 +37,17 @@ ParadigmDescription(
 ParadigmDescription(
     'pistonphone_calibration', 'Pistonphone calibration', 'calibration', [
         (PATH + 'pistonphone_calibration.PistonphoneCalibrationManifest',),
+    ]
+)
+
+
+ParadigmDescription(
+    'amplifier_calibration', 'Amplifier calibration', 'calibration', [
+        (PATH + 'amplifier_calibration.AmplifierCalibrationManifest',),
+        (PATH + 'calibration_mixins.ChannelSettingMixins',),
+        (CORE_PATH + 'microphone_mixins.MicrophoneFFTViewManifest',
+         {'required': True}),
+        (CORE_PATH + 'microphone_mixins.MicrophoneSignalViewManifest',
+         {'required': True}),
     ]
 )
