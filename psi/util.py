f324f4771e13a0333631daba45d3b92335570d37
Brad Buran <bburan@alum.mit.edu>
2022-04-12 09:45:04 -0700

    Fix deprecation of inspect.getargspec

--- psi/util.py
+++ psi/util.py
@@ -25,7 +25,7 @@ def rpc(plugin_name, method_name):
     def wrapper(event):
         plugin = event.workbench.get_plugin(plugin_name)
         f = getattr(plugin, method_name)
-        argnames = inspect.getargspec(f).args
+        argnames = inspect.getfullargspec(f).args
         parameters = {}
         for k, v in event.parameters.items():
             if k in argnames:
