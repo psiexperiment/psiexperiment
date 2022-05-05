ec159f07ea11bf3e933566d8ff8d51217985a4b8
Brad Buran <bburan@alum.mit.edu>
2022-05-05 10:33:50 -0700

    Add some imports to data API

--- psi/data/api.py
+++ psi/data/api.py
@@ -1,6 +1,6 @@
 import enaml
 
-from .plots import FFTContainer, PlotContainer, ResultPlot, ViewBox
+from .plots import FFTContainer, GroupedEpochFFTPlot, PlotContainer, ResultPlot, ViewBox
 
 with enaml.imports():
     from .sink import Sink, SinkWithSource, SinkWithSourceManifest
