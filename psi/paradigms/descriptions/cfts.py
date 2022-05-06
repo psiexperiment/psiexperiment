f22b7b78209cc3609c4c972a8f45ab74666b5c63
Brad Buran <bburan@alum.mit.edu>
2022-05-06 12:48:42 -0700

    Move microphone mixins to a core folder

--- psi/paradigms/descriptions/cfts.py
+++ psi/paradigms/descriptions/cfts.py
@@ -1,36 +1,9 @@
 from psi.experiment.api import ParadigmDescription
+from .core import CORE_PATH, microphone_mixin, microphone_fft_mixin
 
 
 CAL_PATH = 'psi.paradigms.calibration.'
-CORE_PATH = 'psi.paradigms.core.'
 CFTS_PATH = 'psi.paradigms.cfts.'
-
-
-microphone_mixin = {
-    'manifest': CORE_PATH + 'signal_mixins.SignalViewManifest',
-    'attrs': {
-        'id': 'microphone_signal_view',
-        'title': 'Microphone view (time)',
-        'time_span': 4,
-        'time_delay': 0.125,
-        'source_name': 'microphone',
-        'y_label': 'Microphone (dB)'
-    },
-}
-
-
-microphone_fft_mixin = {
-    'manifest': CORE_PATH + 'signal_mixins.SignalFFTViewManifest',
-    'attrs': {
-        'id': 'microphone_fft_view',
-        'title': 'Microphone view (PSD)',
-        'fft_time_span': 0.25,
-        'fft_freq_lb': 5,
-        'fft_freq_ub': 50000,
-        'source_name': 'microphone',
-        'y_label': 'Microphone (dB)'
-    }
-}
 
 
 eeg_mixin = {
