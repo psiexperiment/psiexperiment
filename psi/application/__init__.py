441748aa0c7259b204783ab7282ab4c0bdb71fd1
Brad Buran <info@bradburan.com>
2022-08-01 14:46:12 -0700

    Fix logging init when coloredlogs missing

--- psi/application/__init__.py
+++ psi/application/__init__.py
@@ -79,6 +79,7 @@ def configure_logging(level_console=None, level_file=None, filename=None,
         min_level = min(getattr(logging, level_console), getattr(logging, level_file))
         log.setLevel(min_level)
 
+    fmt = '{levelname:10s}: {threadName:11s} - {name:40s}:: {message}'
     if level_console is not None:
         try:
             level_styles = {
@@ -91,15 +92,13 @@ def configure_logging(level_console=None, level_file=None, filename=None,
             }
             import coloredlogs
             coloredlogs.install(level=level_console, logger=log,
-                                level_styles=level_styles)
+                                level_styles=level_styles, fmt=fmt)
         except ImportError as e:
             stream_handler = logging.StreamHandler()
+            formatter = logging.Formatter(fmt, style='{')
             stream_handler.setFormatter(formatter)
             stream_handler.setLevel(level_console)
             log.addHandler(stream_handler)
-
-    fmt = '{levelname:10s}: {threadName:11s} - {name:40s}:: {message}'
-    formatter = logging.Formatter(fmt, style='{')
 
     if filename is not None and level_file is not None:
         file_handler = logging.FileHandler(filename, 'w', 'UTF-8')
