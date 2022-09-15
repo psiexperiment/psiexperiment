962948b66285ad56af7e7471b7b52313fee8e3f8
Brad Buran <info@bradburan.com>
2022-09-15 15:32:31 -0700

    Add ability to provide custom wrapup function

--- examples/app/launch.py
+++ examples/app/launch.py
@@ -1,19 +1,21 @@
 from psi import set_config
 
 
-def main():
+def main(manifests):
     import logging
     logging.basicConfig(level='ERROR')
 
     from psi.experiment.api import PSIWorkbench
+
     workbench = PSIWorkbench()
 
     set_config('EXPERIMENT', 'demo_experiment')
     io_manifest = 'io_manifest.IOManifest'
-    controller_manifests = ['simple_experiment.ControllerManifest']
-    workbench.register_core_plugins(io_manifest, controller_manifests)
+    workbench.register_core_plugins(io_manifest, manifests)
     workbench.start_workspace('demo')
 
 
 if __name__ == '__main__':
-    main()
+    #manifests = ['simple_experiment.ControllerManifest']
+    manifests = ['startstop_controller.ControllerManifest']
+    main(manifests)
