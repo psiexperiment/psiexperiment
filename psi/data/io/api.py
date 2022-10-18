8f130f1d80defeca1366ebd37fd763b6f259cac1
Brad Buran <buran@ohsu.edu>
2022-10-18 06:46:08 -0700

    Move additional modules out to separate cfts module

--- psi/data/io/api.py
+++ psi/data/io/api.py
@@ -1,13 +1,1 @@
-from pathlib import Path
-from .recording import DirectoryRecording, ZipRecording
-from .summarize_dpoae import isodp_th_criterions
-
-
-def open(path):
-    path = Path(path)
-    if path.suffix == '.zip':
-        return ZipRecording(path)
-    elif path.is_dir():
-        return DirectoryRecording(path)
-    else:
-        raise NotImplementedError
+from .recording import Recording
