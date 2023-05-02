d38b4398c48c0b1e67d04eef996838a7ef70c16c
Brad Buran <buran@ohsu.edu>
2023-05-02 09:57:27 -0700

    Disable quick edit only on Windows

--- psi/application/__init__.py
+++ psi/application/__init__.py
@@ -20,6 +20,29 @@ with enaml.imports():
 
 from psi import get_config, set_config
 
+
+def disable_quick_edit():
+    # From https://stackoverflow.com/questions/73486528/python-script-pausing-in-cmd
+    import win32console as con
+    import signal
+
+    # Missing constants in pywin
+    ENABLE_EXTENDED_FLAGS = 0x0080
+    ENABLE_QUICK_EDIT_MODE = 0x0040
+
+    # Modify console mode to disable quick edit mode
+    h = con.GetStdHandle(con.STD_INPUT_HANDLE)
+    oldMode = h.GetConsoleMode()
+    h.SetConsoleMode((oldMode | ENABLE_EXTENDED_FLAGS) &
+            ~ENABLE_QUICK_EDIT_MODE)
+
+
+# This ensures that running scripts from the command line does not accidentally
+# pause the script.
+if os.name == 'nt':
+    disable_quick_edit()
+
+
 mesg_template = '''
 A critical exception has occurred. While we do our best to prevent these
 issues, they sometimes happen. We are now attempting to shut down the program
