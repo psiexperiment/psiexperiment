b4a8568b8195f382a921b324079e2792772fce58
Brad Buran <buran@ohsu.edu>
2024-08-08 09:30:05 -0700

    Add support for analog out

--- psi/controller/engines/soundcard/__init__.py
+++ psi/controller/engines/soundcard/__init__.py
@@ -4,7 +4,7 @@ log = logging.getLogger(__name__)
 import time
 from threading import Event
 
-from atom.api import Bool, Dict, Float, List, Int, Str, Typed, Value
+from atom.api import Bool, Dict, Enum, Float, List, Int, Str, Typed, Value
 from enaml.core.api import d_
 import numpy as np
 import rtmixer
@@ -20,6 +20,11 @@ STEPSIZE = 4096
 
 
 class SoundcardHardwareAIChannel(HardwareAIChannel):
+
+    channel = d_(Int()).tag(metadata=True)
+
+
+class SoundcardHardwareAOChannel(HardwareAOChannel):
 
     channel = d_(Int()).tag(metadata=True)
 
@@ -43,9 +48,11 @@ class SoundcardEngine(Engine):
     _data = Value()
     _actions = List()
     _hw_ai_buffer = Value()
+    _hw_ao_buffer = Value()
 
     #: Total samples read from the portaudio ringbuffer.
     _total_samples_read = Int()
+    _total_samples_written = Int()
 
     #: These are standard sampling rates in most sound cards. Not all sound
     #: cards may support the highest sampling rates. Calling the result of
@@ -71,48 +78,77 @@ class SoundcardEngine(Engine):
         if ai_channels and ao_channels:
             self._stream = rtmixer.MixerAndRecorder(
                 device=self.device_name,
-                samplerate=96000,
+                samplerate=self.fs,
                 channels=(len(ai_channels), len(ao_channels)),
                 extra_settings=(sd.AsioSettings(ai_chan_number),
                                 sd.AsioSettings(ao_chan_number)),
             )
+            self._configure_ai_cb(ai_channels)
+            self._configure_ao_cb(ao_channels)
+
         elif ai_channels:
             self._stream = rtmixer.Recorder(
                 device=self.device_name,
-                samplerate=96000,
+                samplerate=self.fs,
                 channels=len(ai_channels),
                 extra_settings=sd.AsioSettings(ai_chan_number),
             )
-            elementsize = len(ai_channels) * self._stream.samplesize
-            self._hw_ai_buffer = rtmixer.RingBuffer(elementsize, STEPSIZE * QSIZE)
-            self._actions.append(self._stream.record_ringbuffer(self._hw_ai_buffer))
-            self._threads['hw_ai'] = DAQThread(
-                1e-3,
-                self._stop_requested,
-                lambda: self._hw_ai_callback(len(ai_channels)),
-                name='hw_ai'
-            )
-            self._total_samples_read = 0
-
+            self._configure_ai_cb(ai_channels)
         elif ao_channels:
             self._stream = rtmixer.Mixer(
                 device=self.device_name,
-                samplerate=96000,
+                samplerate=self.fs,
                 channels=len(ao_channels),
                 extra_settings=sd.AsioSettings(ao_chan_number),
             )
+            self._configure_ao_cb(ao_channels)
+
         self._configured = True
 
         super().configure()
 
+    def _configure_ao_cb(self, ao_channels):
+        if isinstance(self._stream.samplesize, tuple):
+            samplesize = self._stream.samplesize[1]
+        else:
+            samplesize = self._stream.samplesize
+        elementsize = len(ao_channels) * samplesize
+        self._hw_ao_buffer = rtmixer.RingBuffer(elementsize, STEPSIZE * QSIZE)
+        self._actions.append(self._stream.play_ringbuffer(self._hw_ao_buffer))
+        self._threads['hw_ao'] = DAQThread(
+            1e-3,
+            self._stop_requested,
+            lambda: self._hw_ao_callback(len(ao_channels)),
+            name='hw_ao'
+        )
+        self._total_samples_written = 0
+
+    def _configure_ai_cb(self, ai_channels):
+        if isinstance(self._stream.samplesize, tuple):
+            samplesize = self._stream.samplesize[0]
+        else:
+            samplesize = self._stream.samplesize
+        elementsize = len(ai_channels) * samplesize
+        self._hw_ai_buffer = rtmixer.RingBuffer(elementsize, STEPSIZE * QSIZE)
+        self._actions.append(self._stream.record_ringbuffer(self._hw_ai_buffer))
+        self._threads['hw_ai'] = DAQThread(
+            1e-3,
+            self._stop_requested,
+            lambda: self._hw_ai_callback(len(ai_channels)),
+            name='hw_ai'
+        )
+        self._total_samples_read = 0
+
     def _hw_ai_callback(self, n_channels):
         while self._hw_ai_buffer.read_available > STEPSIZE:
-            read, buf1, buf2 = self._hw_ai_buffer.get_read_buffers(STEPSIZE)
-            if read != STEPSIZE:
-                raise ValueError
-            if buf2:
-                raise ValueError
-            samples = np.frombuffer(buf1, dtype='float32')
+            samples = np.frombuffer(self._hw_ai_buffer.read(), dtype='float32')
+            #read, buf1, buf2 = self._hw_ai_buffer.get_read_buffers(STEPSIZE)
+            #if read != STEPSIZE:
+                #raise ValueError
+            #if buf2:
+                #raise ValueError
+            #print(samples)
+            #samples = np.frombuffer(samples, dtype='float32')
             samples.shape = -1, n_channels
 
             data = PipelineData(samples.T, fs=96000, s0=self._total_samples_read)
@@ -120,10 +156,36 @@ class SoundcardEngine(Engine):
                 #if channel_name == name:
                 cb(data)
 
-            self._total_samples_read += STEPSIZE
-            self._hw_ai_buffer.advance_read_index(STEPSIZE)
+            self._total_samples_read += len(samples)
+
+    def _get_hw_ao_samples(self, offset, samples):
+        channels = self.get_channels('analog', 'output', 'hardware')
+        data = np.empty((len(channels), samples), dtype=np.double)
+        for channel, ch_data in zip(channels, data):
+            channel.get_samples(offset, samples, out=ch_data)
+        return data
+
+    def _hw_ao_callback(self, n_channels=None):
+        if (n := self._hw_ao_buffer.write_available) <= 0:
+            return
+        data = self._get_hw_ao_samples(self._total_samples_written, n)
+        buffer = data.astype('float32').T.tobytes()
+        result = self._hw_ao_buffer.write(buffer)
+
+    def play(self, data, name, start=0, allow_belated=True):
+        buffer = data.astype('float32').T.tobytes()
+        return self._stream.play_buffer(buffer, channels=[1])
+
+    #: Size of buffer (in seconds). This defines how much data is pregenerated
+    #: for the buffer before starting acquisition. This is important because
+    hw_ao_buffer_size = d_(Float(10)).tag(metadata=True)
+
+    def get_buffer_size(self, name):
+        return self.hw_ao_buffer_size
 
     def start(self):
+        # Preload with data
+        self._hw_ao_callback()
         for thread in self._threads.values():
             thread.start()
         self._stream.start()
@@ -131,7 +193,6 @@ class SoundcardEngine(Engine):
     def stop(self):
         if not self._configured:
             return
-
         self._stop_requested.set()
         self.complete()
         self._configured = False
