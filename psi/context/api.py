dc45bb27b4d84df487ad12ef0829e35883f1680f
Brad Buran <buran@ohsu.edu>
2025-05-23 13:54:42 -0700

    Make choice functions part of API

--- psi/context/api.py
+++ psi/context/api.py
@@ -1,3 +1,8 @@
+from .choice import (
+    ascending, descending, pseudorandom, exact_order, shuffled_set,
+    counterbalanced,
+)
+
 from .context_item import (
     BoolParameter, ContextGroup, ContextMeta, ContextRow, EnumParameter, Expression,
     FileParameter, OrderedContextMeta, Parameter, Result, UnorderedContextMeta
