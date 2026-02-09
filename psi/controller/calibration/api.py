798107ae1ca313877fdbafb783a0a3cfb6407916
Brad Buran <buran@ohsu.edu>
2026-02-09 10:50:33 -0800

    Fix import

--- psi/controller/calibration/api.py
+++ psi/controller/calibration/api.py
@@ -2,10 +2,13 @@ from .acquire import acquire
 from .calibrate import (
     BaseCalibrate, ChirpCalibrate, ClickCalibrate, ToneCalibrate
 )
-from .calibrate_manifest import (
-    BaseCalibrateManifest, ChirpCalibrateManifest, ClickCalibrateManifest,
-    ToneCalibrateManifest
-)
 from .chirp import chirp_sens
 from .plugin import CalibrationPlugin
 from .tone import process_tone, tone_power_conv, tone_sens
+
+import enaml
+with enaml.imports():
+    from .calibrate_manifest import (
+        BaseCalibrateManifest, ChirpCalibrateManifest, ClickCalibrateManifest,
+        ToneCalibrateManifest
+    )
